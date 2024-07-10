95b6b799dfb08f201eef8b0a04adfaca64a2b5ca
CheonMinJae <cmjcmj1011@gmail.com>
2024-07-11 00:39:29 +0900

    초기 세팅

---
+++ README.md
@@ -0,0 +1,11 @@
+## 초기 세팅
+
+`git clone https://github.com/GDSC-Hongik/rental-system-mango-web.git`  
+
+를 통해 프로젝트를 클론합니다.  
+  
+`npm install`  
+을 통해 필요한 package 를 다운받습니다.  
+
+`npm run start`  
+를 하면 프로젝트를 실행할 수 있습니다.
---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+// @ts-check
+import path from 'path';
+import { fileURLToPath } from 'url';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+  },
+);
