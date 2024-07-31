49d335014f60e9ad0cea90202cadd7e0932d0fbf
CheonMinJae <cmjcmj1011@gmail.com>
2024-08-01 01:54:31 +0900

    path alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: [
+			{ find: '@apis', replacement: path.resolve(__dirname, 'src/apis') },
+			{
+				find: '@components',
+				replacement: path.resolve(__dirname, 'src/components'),
+			},
+			{ find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
+			{ find: '@routes', replacement: path.resolve(__dirname, 'src/routes') },
+		],
+	},
+
 	server: {
 		host: '0.0.0.0',
 		port: 3000, // 포트 설정. 기본값은 5173
