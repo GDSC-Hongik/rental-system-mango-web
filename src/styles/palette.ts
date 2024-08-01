62110ee3b084cf99dfa2f237fcde574c5f37e23d
sumnunus <klslml123@naver.com>
2024-08-01 20:03:42 +0900

    팔레트 및 타이포그래피 셋팅

---
+++ src/styles/palette.ts
@@ -0,0 +1,18 @@
+export const palette = {
+	gradient: {
+		main_linear_1:
+			'linear-gradient(180deg, #1800CF 21%, rgba(24, 0, 207, 0.3) 78%, rgba(24, 0, 207, 0) 100%);',
+		main_linear_2:
+			'linear-gradient(180deg, rgba(202, 205, 225, 0) 25.6%, #CACDE1 79.1%);',
+	},
+	background_color: '#F7F8FF',
+	main_color: '#1800CF',
+	main_color_lighter: '#bfc7e4',
+	dark_color: '#362b48',
+	bluegray_color: '#cacde1',
+	black: '#000000',
+	red: '#ff5c5c',
+	white: '#ffffff',
+	gray: '#BDBDBD',
+	blue: '#1833DB',
+} as const;
