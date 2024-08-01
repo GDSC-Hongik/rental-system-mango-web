c5067ad82441bc54447ff7145189e3c5a8e29ceb
CheonMinJae <cmjcmj1011@gmail.com>
2024-08-02 01:11:13 +0900

    react query 설치 및 linter 설정

--- eslint.config.js
+++ eslint.config.js
@@ -2,21 +2,25 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import tseslint, { plugin } from 'typescript-eslint';
+import pluginQuery from '@tanstack/eslint-plugin-query';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default tseslint.config(
-	eslint.configs.recommended,
-	...tseslint.configs.strictTypeChecked,
-	{
-		languageOptions: {
-			parserOptions: {
-				project: true,
-				tsconfigRootDir: __dirname,
+export default [
+	...tseslint.config(
+		eslint.configs.recommended,
+		...tseslint.configs.strictTypeChecked,
+		{
+			languageOptions: {
+				parserOptions: {
+					project: true,
+					tsconfigRootDir: __dirname,
+				},
 			},
+			ignores: ['eslint.config.js', 'vite.config.ts'],
 		},
-		ignores: ['eslint.config.js', 'vite.config.ts'],
-	},
-);
+	),
+	...pluginQuery.configs['flat/recommended'],
+];
