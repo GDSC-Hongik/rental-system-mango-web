c4f5b82f68784ebe7a32c4bffd85f29f4edcaa11
CheonMinJae <cmjcmj1011@gmail.com>
2024-08-03 01:55:08 +0900

    query client axios client 설정, axios 는 임시 api 때문에 withCredential 을 주석 처리했습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Router from './routes';
 import ZustandTest from './simpleTest/ZustandTest';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import ApiTestCompoent from '@components/ApiTest';
+
+const queryClient: QueryClient = new QueryClient({
+	/* options */
+});
 
 function App() {
 	return (
 		<>
-			<div className='App'>hello, react</div>
+			<QueryClientProvider client={queryClient}>
+				{/* zustand 테스트를 위한 컴포넌트 */}
+				<ZustandTest />
+				<Router />
+				<ApiTestCompoent />
 
-			{/* zustand 테스트를 위한 컴포넌트 */}
-			<ZustandTest />
-			<Router />
+				<ReactQueryDevtools initialIsOpen={false} />
+			</QueryClientProvider>
 		</>
 	);
 }
