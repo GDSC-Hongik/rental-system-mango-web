8bf6eee9041af8578aabe10529c408584a3c9976
sumnunus <klslml123@naver.com>
2024-08-14 23:37:20 +0900

    물품 선택 버튼 컴포넌트

--- src/components/RentList/RentList.tsx
+++ src/components/RentList/RentList.tsx
@@ -17,7 +17,7 @@ const WhiteBox: React.FC<WhiteBoxProps> = ({
 				height: ${height};
 				background-color: #ffffff;
 				border-radius: 20px;
-				box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+				box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.1);
 			`}
 		/>
 	);
