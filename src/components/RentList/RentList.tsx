fe2bfeef644451b8c01acf5e51f314087d345817
sumnunus <klslml123@naver.com>
2024-08-15 00:03:12 +0900

    물품 선택 버튼 컴포넌트

---
+++ src/components/RentList/RentList.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { css } from '@emotion/react';
+
+interface WhiteBoxProps {
+	width?: string;
+	height?: string;
+}
+
+const WhiteBox: React.FC<WhiteBoxProps> = ({
+	width = '209px',
+	height = '139px',
+}) => {
+	return (
+		<div
+			css={css`
+				width: ${width};
+				height: ${height};
+				background-color: #ffffff;
+				border-radius: 20px;
+				box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+			`}
+		/>
+	);
+};
+
+export default WhiteBox;
